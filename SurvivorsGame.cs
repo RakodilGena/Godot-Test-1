2d247718dc8c929293e167fee2dd753371b8cfae
RakodilGena <rakodilgenathefirst@gmail.com>
2026-07-16 16:37:24 +0300

    add gameover screen

--- SurvivorsGame.cs
+++ SurvivorsGame.cs
@@ -5,11 +5,14 @@ namespace GettingStartedWithGodot4;
 public partial class SurvivorsGame : Node2D
 {
     private static readonly PackedScene MobScene = GD.Load<PackedScene>("res://mob.tscn");
+    
     private PathFollow2D _pathFollow = null!;
+    private CanvasLayer _gameOver = null!;
 
     public override void _Ready()
     {
         _pathFollow = GetNode<PathFollow2D>("%MobSpawnPathFollow");
+        _gameOver = GetNode<CanvasLayer>("%GameOverScreen");
     }
 
     private void OnMobSpawnTimerTimeout()
@@ -25,5 +28,11 @@ public partial class SurvivorsGame : Node2D
         
         (mob as Mob)?.GlobalPosition = _pathFollow.GlobalPosition;
         AddChild(mob);
+    }
+
+    private void OnPlayerHealthDepleted()
+    {
+        _gameOver.Visible = true;
+        GetTree().Paused = true;
     }
 }
